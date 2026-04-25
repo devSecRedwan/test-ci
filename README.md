5a544b8feecba2db630839930be57aaa5f2fa9b7
devSecRedwan <smredwan.info@gmail.com>
2026-04-25 09:52:33 +0600

    Build calculator with sum, sub and mul function

---
+++ README.md
@@ -0,0 +1,13 @@
+# Simple Calculator
+This is a simple calcultor made by HTML, CSS and JS only.
+
+### Prerequisites
+- node
+- npm
+
+### Steps of running this apps
+- clone this repository using git clone
+- run npm install
+- Go live server in vs code
+
+and use it.
---
+++ src/script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculateResult() {
+  try {
+    if (display.value === "") return; // Do nothing if screen is empty
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
